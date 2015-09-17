eb0e7b98c95a087256fc469937776e80ddaefea4
Anaid Romero <anirom89@gmail.com>
2015-09-17 17:38:07 -0500

    Suma logaritmica de n numeros primos y su ratio

--- ps1a.py
+++ ps1a.py
@@ -8,19 +8,23 @@
 n = 2
 primeNumber = 0
 
+# No se detendra hasta que llegue a 1000 numeros primos
 while primeNumber < 1000:
     x = n
     contador = 0
+    # Verifica si es una division exacta
     number = n%x
     if number == 0:
         contador += 1
         x -= 1
+        # Disminuye el valor del divisor
         for x in range(x,0,-1):
             number = n%x
             if number == 0:
                 contador += 1
             else:
                 x -= 1
+    # Verifica que solo tenga dos divisiones exactas, que seria entre 1 y su mismo numero
     if contador == 2:
         print(n)
         primeNumber += 1
