82a4152f76953335122d4afb18952303f0e90552
Anaid Romero <anirom89@gmail.com>
2015-09-17 14:40:27 -0500

    Calculo de los primeros 1000 numeros primos

--- ps1a.py
+++ ps1a.py
@@ -8,20 +8,20 @@
 n = 2
 primeNumber = 0
 
-while primeNumber < 1000:
+while primeNumber < 1000: # Seguirá corriendo hasta que llegue a 1000 numeros primos
     x = n
     contador = 0
-    number = n%x
+    number = n%x # Verifica si es una division exacta
     if number == 0:
         contador += 1
         x -= 1
-        for x in range(x,0,-1):
+        for x in range(x,0,-1): # Va disminuyendo el valor del divisor
             number = n%x
-            if number == 0:
+            if number == 0: # Verifica si es exacta o no la division
                 contador += 1
             else:
                 x -= 1
-    if contador == 2:
+    if contador == 2: # Se usa para verificar que sólo tenga dos divisiones exactas. Que fuera entre 1 y el mismo numero
         print(n)
         primeNumber += 1
         n += 1
