a45ecc581b72dff033cb04d6678f2709def71bfe
Anaid Romero <anirom89@gmail.com>
2015-10-05 20:20:52 -0500

    Takes growth rate as list, and the function return a list with the retirment fund

--- PS4/ps4.py
+++ PS4/ps4.py
@@ -19,13 +19,37 @@ def nestEggFixed(salary, save, growthrate, years):
         return retirementfund
 
     else:
-        return "Verifica los rangos de los porcentajes"
+        return "Check the ranges of percentages"
 
+def nestEggVariable(salary, save, growthrates):
+    years = len(growthrates)
+    retirementfund = []
+    for i in range(1,years+1):
+        if i == 1:
+            retirement = salary*save*0.01
+            retirementfund.insert(i-1, retirement)
+        else:
+            retirement = retirementfund[i-2] *(1+0.01*growthrates[i-1]) + retirementfund[0]
+            retirementfund.insert(i-1,retirement)
+
+    return retirementfund
 
 def testNestEggFixed():
 
+    print("Retirement fund with salary, save, growthrate, years as inputs")
     print(nestEggFixed(10000,10,15,5))
     print(nestEggFixed(5000,5,3,2))
     print(nestEggFixed(4000,0,200,7))
 
+def testNestEggVariable():
+
+    print("\n Retirement fund with salary, save and growthrate list as inputs ")
+    print(nestEggVariable(10000,10,[3,4,5,0,3]))
+    print(nestEggVariable(5000,5,[5,7,6]))
+    print(nestEggVariable(4000,3,[3,6,9,12]))
+
 testNestEggFixed()
+testNestEggVariable()
+
+
+
